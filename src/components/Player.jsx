d619b146fa7481f324aedad1df3ccf4bdb93e30f
Sophie <sallysophie@hotmail.de>
2024-11-07 14:22:36 +0100

    refactor App

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
-const Player = ({ initialName, symbol, isActive }) => {
+const Player = ({ initialName, symbol, isActive, onChangeName }) => {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleClick = () => {
     setIsEditing((prevIsEditing) => !prevIsEditing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   };
 
   const handleChange = (event) => {
